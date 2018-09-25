955a7bcde5e750eef535a62a6c77c051b3a74044
Ken Petti <kennethpetti+github@gmail.com>
2018-09-24 21:59:38 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# conduit
+
+Golang app created with generator-go and yeoman.
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import(
+"fmt"
+"github.com/citizenken/conduit/hello"
+)
+
+func main() {
+	fmt.Println(hello.BuildHello())
+}
